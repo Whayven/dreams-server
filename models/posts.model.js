dcd6579640d8af861dbd0952155481731a60572d
Wayne <foster_wayne@columbusstate.edu>
2020-01-23 11:01:22 -0500

    finished routes and models

---
+++ models/posts.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
